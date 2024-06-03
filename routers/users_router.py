079900199b2e5d4c5fc3b0706b148e9998f8805d
Bhav Beri <bhavberi@gmail.com>
2024-06-03 11:00:21 +0530

    Added extend_cookie API endpoint

--- routers/users_router.py
+++ routers/users_router.py
@@ -16,10 +16,13 @@ async def login(
     request: Request,
     response: Response,
     form_data: OAuth2PasswordRequestForm = Depends(),
-    access_token_ims_app: str = Depends(check_current_user),
+    access_token_ims_app: str | None = Depends(check_current_user),
 ):
-    if access_token_ims_app:
-        response.delete_cookie("access_token_ims_app")
+    if access_token_ims_app is not None:
+        raise HTTPException(
+            status_code=status.HTTP_304_NOT_MODIFIED,
+            detail="Already Logged In!",
+        )
 
     auth_success, user_data = authenticate_user(form_data.username, form_data.password)
     if not auth_success:
@@ -43,7 +46,7 @@ async def login(
 
 # User Logout
 @router.post("/logout", status_code=status.HTTP_202_ACCEPTED)
-async def logout(response: Response, current_user=Depends(check_current_user)):
+async def logout(response: Response, current_user: str | None = Depends(check_current_user)):
     if current_user is None:
         raise HTTPException(status_code=401, detail="Not Logged In!")
     response.delete_cookie("access_token_ims_app")
@@ -57,3 +60,16 @@ async def read_users_me(user_data=Depends(get_current_user)):
 
     role_data = get_user_roles(email)
     return {**user_data, **role_data}
+
+@router.post("/extend_cookie", status_code=status.HTTP_200_OK)
+async def extend_cookie(
+    request: Request,
+    response: Response,
+    user_data=Depends(get_current_user),
+):
+    # Extend the access token/expiry time
+    new_access_token = create_access_token(data={"username": user_data["username"], "email": user_data["email"]})
+    response.set_cookie(
+        key="access_token_ims_app", value=new_access_token, httponly=True
+    )
+    return {"message": "Cookie Extended Successfully"}
